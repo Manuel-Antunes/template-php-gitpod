04581d004f144d33b2f68618c40096afc251d08d
Manuel Nascimento <nerdbroz2@gmail.com>
2021-08-07 02:41:25 +0000

    feat: template basic finished

---
+++ src/index.php
@@ -0,0 +1,15 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Template PHP Gitpod</title>
+</head>
+<body>
+  <h1>HELLO WORLD!</h1>
+  <h2>BORA CODAR!</h2>
+</body>
+</html>
